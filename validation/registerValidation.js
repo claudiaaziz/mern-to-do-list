e58b61f5c0873f6e7f41c951d4cb0a1c0dc02fb9
claudiaaziz <claudiahannahh@gmail.com>
2024-04-06 14:13:27 -0400

    Add User Validations

---
+++ validation/registerValidation.js
@@ -0,0 +1,40 @@
+const Validator = require('validator');
+const isEmpty = require('./isEmpty');
+
+const validateRegisterInput = (data) => {
+  const errors = {};
+
+  // check the email field
+  if (isEmpty(data.email)) {
+    errors.email = 'Email field cannot be empty.';
+  } else if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid. Please provide a valid email.';
+  }
+
+  // check the name field
+  if (!Validator.isLength(data.name, { min: 2, max: 30 }) || isEmpty(data.name)) {
+    errors.name = 'Name is invalid';
+  }
+
+  // check the password field
+  if (isEmpty(data.password)) {
+    errors.password = 'Password must be at least 8 characters long.';
+  } else if (!Validator.isLength(data.password, { min: 8, max: 150 })) {
+    errors.password = 'Password must be between 8 and 150 characters long';
+  }
+
+  // check the confirmed password field
+  if (
+    isEmpty(data.confirmPassword) ||
+    !Validator.equals(data.password, data.confirmPassword)
+  ) {
+    errors.password = 'Password and Confirm Password must match.';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
+
+module.exports = validateRegisterInput;
