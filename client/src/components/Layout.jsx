2fe2d12ac6e573a32e4fff44cad7450406ac44a2
claudiaaziz <claudiahannahh@gmail.com>
2024-04-07 17:09:14 -0400

    Setup global context

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,9 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import AuthBox from './AuthBox';
+import { useGlobalContext } from '../context/GlobalContext';
 
 const Layout = () => {
-  return (
+  const { fetchingUser } = useGlobalContext();
+
+  return fetchingUser ? (
+    <div className='loading'>
+      <h1>Loading...</h1>
+    </div>
+  ) : (
     <BrowserRouter>
       <Header />
       <Routes>
