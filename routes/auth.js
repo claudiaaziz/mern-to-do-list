e2501d9476e59b96ec56d0c14745cc35b4fe5051
claudiaaziz <claudiahannahh@gmail.com>
2024-04-06 21:01:44 -0400

    Add auth to users

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const validateRegisterInput = require('../validation/registerValidation');
+const jwt = require('jsonwebtoken');
+const requiresAuth = require('../middleware/permissions');
 
 // @route   GET /api/auth/test
 // @desc    Test the Auth route
@@ -44,11 +46,66 @@ router.post('/register', async (req, res) => {
     const savedUser = await newUser.save();
 
     // return the new user
-    return res.json(savedUser);
+    const userToReturn = { ...savedUser._doc };
+    delete userToReturn.password;
+    return res.json(userToReturn);
   } catch (err) {
     console.log(err);
     return res.status(500).send(err.message);
   }
+});
+
+// @route   POST /api/auth/login
+// @desc    Login user and return a access token
+// @accesss Public
+router.post('/login', async (req, res) => {
+  try {
+    // check for the user
+    const user = await User.findOne({
+      email: new RegExp('^' + req.body.email + '$', 'i'),
+    });
+
+    if (!user) res.status(400).json({ error: 'Incorrect email or password.' });
+
+    const passwordMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordMatch)
+      res.status(400).json({ error: 'Incorrect email or password.' });
+
+    const payload = { userId: user._id };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+
+    res.cookie('access-token', token, {
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+    });
+
+    const userToReturn = { ...user._doc };
+    delete userToReturn.password;
+
+    return res.json({
+      token,
+      user: userToReturn,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send(err.message);
+  }
+});
+
+// @route   GET /api/auth/current
+// @desc    Return currently authed user
+// @accesss Private
+router.get('/current', requiresAuth, (req, res) => {
+  if (!req.user) return res.status(401).send('Unauthorized')
+  
+  return res.send(req.user)
 });
 
 module.exports = router;
