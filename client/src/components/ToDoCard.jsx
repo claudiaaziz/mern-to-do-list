0a8dc670647e95aacc76b91cb529ea21a579d66d
claudiaaziz <claudiahannahh@gmail.com>
2024-04-17 21:04:56 -0400

    Marking todos as complete/ incomplete on the frontend

--- client/src/components/ToDoCard.jsx
+++ client/src/components/ToDoCard.jsx
@@ -1,10 +1,13 @@
 import { useRef, useState } from 'react';
 import '../styles/toDoCard.scss';
+import axios from 'axios';
+import { useGlobalContext } from '../context/GlobalContext';
 
 const ToDoCard = ({ toDo }) => {
   const [content, setContent] = useState(toDo.content);
   const [isEditing, setIsEditing] = useState(false);
   const input = useRef(null);
+  const { toDoComplete, toDoIncomplete } = useGlobalContext();
 
   const onEdit = (e) => {
     e.preventDefault();
@@ -13,14 +16,34 @@ const ToDoCard = ({ toDo }) => {
   };
 
   const onCancel = (e) => {
-    if (e) e.preventDefault()
-    setIsEditing(false)
-    setContent(toDo.content)
-  }
+    if (e) e.preventDefault();
+    setIsEditing(false);
+    setContent(toDo.content);
+  };
+
+  const markAsComplete = (e) => {
+    e.preventDefault();
+
+    axios.put(`/api/todos/${toDo._id}/complete`).then((res) => {
+      toDoComplete(res.data);
+    });
+  };
+
+  const markAsIncomplete = (e) => {
+    e.preventDefault();
+
+    axios.put(`/api/todos/${toDo._id}/incomplete`).then((res) => {
+      toDoIncomplete(res.data);
+    });
+  };
 
   return (
     <div className={`todo ${toDo.complete && 'todo--complete'}`}>
-      <input type='checkbox' checked={toDo.complete} />
+      <input
+        type='checkbox'
+        checked={toDo.complete}
+        onChange={!toDo.complete ? markAsComplete : markAsIncomplete}
+      />
       <input
         type='text'
         ref={input}
