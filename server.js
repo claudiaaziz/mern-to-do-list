a2f362bc63a4396e5833f5ebcf72859903e74f47
claudiaaziz <claudiahannahh@gmail.com>
2024-04-19 20:43:39 -0400

    Prep for deployment

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser')
-const cors = require('cors');
+const cookieParser = require('cookie-parser');
+// const cors = require('cors');
+const path = require('path');
 
 const authRoute = require('./routes/auth');
-const toDosRoute = require('./routes/todos')
+const toDosRoute = require('./routes/todos');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(cookieParser())
-app.use(cors());
+app.use(cookieParser());
+// app.use(cors());
 
 app.get('/api', (req, res) => {
   res.send('Express Server 🩷');
@@ -20,6 +21,12 @@ app.get('/api', (req, res) => {
 
 app.use('/api/auth', authRoute);
 app.use('/api/todos', toDosRoute);
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
